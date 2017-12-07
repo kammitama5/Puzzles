fffbc636c920839725f4f683d1ab99906390ea3c
kammitama5 <krystal.maughan@gmail.com>
2017-12-07 13:59:03 -0800

    Add files via upload

---
+++ Arrays_Strings/ArrayQuestions.py
@@ -0,0 +1,43 @@
+"""
+1.
+Implement an algorithm to determine if a string has all 
+unique characters
+"""
+
+"""
+2.
+Write code to reverse a C-style string.
+"""
+
+"""
+3.
+Design an algorithm to write code to remove the duplicate
+characters in a string without using any additional buffer.
+Note: One or two of the additional variables are fine.
+An extra copy of the array is not.
+Write the test cases for this method 
+"""
+
+"""
+4.
+Write a method to decide if two strings are anagrams or not 
+"""
+
+"""
+5.
+Write a method to replace all spaces in a string with '%20'
+"""
+
+"""
+6.
+Write an algorithm such that if an element in an MxN matrix is 0, 
+its entire row and column is set to 0
+"""
+
+"""
+7.
+Assume you have a method isSubString which checks if one word 
+is a substring of another. Given two strings, s1 and s2, write 
+code to check if s2 is a rotation of s1 using only one call to 
+to isSubstring (ie waterbottle is a rotation of erbottlewat)
+"""
