d7a8ff1256f88c89baa57c76758591f83a1a56a7
kammitama5 <krystal.maughan@gmail.com>
2017-12-07 15:55:54 -0800

    reverse C style works

---
+++ Arrays_Strings/reverseCstyle.py
@@ -0,0 +1,41 @@
+"""
+2.
+Write code to reverse a C-style string. Name it reverse.
+"""
+
+def reverse(string):
+  try:
+    ## exception handling is done by concatenating input with string
+	## will throw type-error mismatch if not a string
+    string + ""
+	## base case 0 or 1 length 
+    if (len(string) <= 1):
+      return string
+	## pop and push to array, and join the chars of the array
+    else:
+     arr = []
+     a = list(string)
+     while len(a) > 0:
+       arr.append(a.pop())
+     b =  "".join(arr)
+     return b
+  except:
+    return "Exception: Not of type string"
+    
+    
+## what is input, what is output 
+## string-> string 
+## edge cases -> string is empty, string has one letter, string is not 
+## of correct type -> exception handling
+## comments and properly name variables 
+## what is most efficient? (don't use .reverse)
+
+
+# tests 
+print reverse(0)
+print reverse([1,2,3])
+print reverse("")
+print reverse(" ")
+print reverse("a")
+print reverse("aheloloo")
+print reverse("Hi my name is Krystal")
